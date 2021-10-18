f7930d235dd30ce5f415b6893b5266d4d2080b84
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-10-18 20:20:57 +0300

    1 commit master

---
+++ lab4.3/lab4.3.cpp
@@ -0,0 +1,59 @@
+﻿// Lab4.3.cpp
+// Гордій Олег
+// Лабораторна робота № 4.3
+// Табуляція функції, заданої формулою: функція з параметрами
+// Варіант 3
+
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+#include <math.h>
+#include <iomanip>
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+    double x;
+    double xp;
+    double xk;
+    double dx;
+    double F; //формула
+
+    cout << " a = " << endl; cin >> a;
+    cout << " b = " << endl; cin >> b;
+    cout << " c = " << endl; cin >> c;
+    cout << " xp = " << endl; cin >> xp;
+    cout << " xk = " << endl; cin >> xk;
+    cout << " dx = " << endl; cin >> dx;
+
+    cout << fixed;
+    cout << "--------------------------------------------------" << endl;
+    cout << "|" << setw(5) << "F" << " |"
+        << setw(9) << "xp" << " |" << setw(13) << "xk" << " |" << setw(17) << "dx" << " |" << endl;
+    cout << "--------------------------------------------------" << endl;
+    x = xp;
+    while (x <= xk)
+    {
+        if (a < 0 && c != 0)
+            F = a * pow(x, 2) + b * x + c;
+        else
+            if (a > 0 && c == 0)
+                F = (-a) / (x - c);
+            else
+                F = a * (x - c);
+
+        cout << "|" << setw(7) << setprecision(2) << F
+            << " |" << setw(10) << setprecision(3) << xp
+            << " |" << setw(13) << setprecision(4) << xk
+            << " |" << setw(16) << setprecision(5) << dx
+            << " |" << endl;
+
+        x += dx;
+    }
+    cout << "--------------------------------------------------" << endl;
+    cin.get();
+    return 0;
+}
